db355c3b570528641d50302b27b15aef2665c4d6
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-10-11 15:20:53 +0100

    edit done

--- controller.rb
+++ controller.rb
@@ -14,6 +14,29 @@ get '/students/new' do
   erb (:new)
 end
 
+get '/students/:id' do
+  @student = Student.find(params['id'])
+  erb ( :show )
+end
+
+get '/students/:id/edit' do
+  @student = Student.find(params['id'])
+  @houses = House.all()
+  erb ( :edit )
+end
+
+post '/students/:id' do
+  @student = Student.new(params)
+  @student.update()
+  erb ( :update )
+end
+
+post '/students/:id/delete' do
+  @student = Student.find(params['id'])
+  @student.delete()
+  erb ( :delete )
+end
+
 post '/students' do
   @student = Student.new(params)
   @student.save()
